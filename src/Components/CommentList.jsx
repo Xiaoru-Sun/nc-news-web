7ed933b221222cc6f59232204d5678c3d4018049
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-07 14:42:53 +0100

    extracts axios functions to app.js, adds CommentCard

--- src/Components/CommentList.jsx
+++ src/Components/CommentList.jsx
@@ -1,7 +1,39 @@
-function CommentList() {
+import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+import CommentCard from "./CommentCard";
+import { fetchComments } from "../utils/app";
+
+function CommentList(props) {
+  const { article_id } = props;
+  const [comments, setComments] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchComments(article_id, setLoading, setComments, setError);
+  }, [article_id]);
+
   return (
     <>
-      <div>Comments are coming soon</div>
+      {loading && (
+        <ReactLoading
+          className="loading"
+          type="spinningBubbles"
+          color="blue"
+        ></ReactLoading>
+      )}
+
+      <ul className="commentcard-container">
+        {comments.map((comment) => {
+          return (
+            <li className="commentcard-list" key={comment.comment_id}>
+              <CommentCard comment={comment}></CommentCard>
+            </li>
+          );
+        })}
+      </ul>
+
+      {error && <p>Error!</p>}
     </>
   );
 }
